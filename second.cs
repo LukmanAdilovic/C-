788125ebdff811bb26fb6faa678b40141ec65b11
Lukman <66786540+LukmanAdilovic@users.noreply.github.com>
2020-06-17 13:17:55 +0200

    commit
    Getting User Input 
    Building a Calculator
    Building a Mad Lib

---
+++ second.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace second.cs
+{
+    class second
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age);
+
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Product is " + (num1 + num2));
+
+            string color, noun, carName;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a noun: ");
+            noun = Console.ReadLine();
+
+            Console.Write("Enter name of car: ");
+            carName = Console.ReadLine();
+
+            Console.WriteLine("Sky is " + color);
+            Console.WriteLine(noun + " is fast");
+            Console.WriteLine("I like " + carName);
+
+            Console.ReadLine();
+        }
+    }
+}
