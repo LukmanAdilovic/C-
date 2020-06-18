fce7b0aaae5769f412bb59e5cd3a333a8baf5373
Lukman <66786540+LukmanAdilovic@users.noreply.github.com>
2020-06-18 15:26:34 +0200

    commit
    commit

--- fifth.cs
+++ fifth.cs
@@ -18,4 +18,19 @@ namespace project.cs
             Console.ReadLine();
         }
     }
+    
+    
+        class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
 }
