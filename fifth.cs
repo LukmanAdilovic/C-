c498c79478b1bdf30ee2ff964a96a0ddab06d708
Lukman <66786540+LukmanAdilovic@users.noreply.github.com>
2020-06-18 15:25:13 +0200

    commit
    commit

---
+++ fifth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace project.cs
+{
+    class fifth
+    {
+        //classes and constructors
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("Lord of the Ring", "Tolkein", 700);
+            
+
+            Console.WriteLine(book2.author);
+            Console.ReadLine();
+        }
+    }
+}
